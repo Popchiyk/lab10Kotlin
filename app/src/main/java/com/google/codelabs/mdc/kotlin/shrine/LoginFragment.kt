3169e45661b483aa2980e1607f9ddad463899d0b
Popchiyk <roma240103@gmail.com>
2023-04-17 22:48:20 +0300

    first

---
+++ app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/LoginFragment.kt
@@ -0,0 +1,18 @@
+package com.google.codelabs.mdc.kotlin.shrine
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.shr_login_fragment, container, false)
+
+        return view
+    }
+}
